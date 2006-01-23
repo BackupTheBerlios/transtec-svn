8126d25b0c763f5036926ad7ad2d98237262e37c
sengler <sengler@09e82f7e-ea0a-0410-8a0f-ac04ed4c31c5>
2006-01-23 13:36:20 +0000

    Erreur de constructeur

--- trunk/donnees/Incident.java
+++ trunk/donnees/Incident.java
@@ -20,8 +20,7 @@ public class Incident {
 	public final static int EN_COURS = 1;
 	public final static int TRAITE = 2;
 	
-	public Incident(Integer id,Integer idColis, Timestamp date, Integer etat, String description, Integer idUtilisateur, Integer type){
-		this.id=id;
+	public Incident(Integer idColis, Timestamp date, Integer etat, String description, Integer idUtilisateur, Integer type){
 		this.idColis=idColis;
 		this.date=date;
 		this.etat=etat;
