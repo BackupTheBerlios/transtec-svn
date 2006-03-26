58580c988a4e4fba89504ef453ddfc56c2f529b3
sengler <sengler@09e82f7e-ea0a-0410-8a0f-ac04ed4c31c5>
2006-03-26 11:10:17 +0000

    finalisation de la fenetre de warning

--- trunk/ihm/FenetreValidation.java
+++ trunk/ihm/FenetreValidation.java
@@ -1,6 +1,7 @@
 package ihm;
 
 import java.awt.*;
+
 import javax.swing.*;
 import javax.swing.JDialog;
 import javax.swing.event.ChangeEvent;
@@ -14,25 +15,35 @@ public class FenetreValidation extends JDialog implements ChangeListener{
 	private Bouton valider, annuler;
 	private boolean resultat=false, blocage=true;
 	private JLabel labelMessage = new JLabel();
-	JPanel panel = new JPanel(new BorderLayout());
+	//JPanel panel = new JPanel(new BorderLayout());
 
 	public FenetreValidation(String message){
+		setBounds(347,242,330,283);
+		setUndecorated(true);
+		FenetreType fenetre=new FenetreType("images/preparation/validation.png");
+		setContentPane(fenetre);
+		// Création de la police
+		Font font=new Font("Verdana", Font.BOLD, 12);
 		//setTitle("Validation");
-		labelMessage.setText(message);
+		/*labelMessage.setText(message);
 		labelMessage.setHorizontalTextPosition(SwingConstants.CENTER);
 		labelMessage.setVerticalTextPosition(SwingConstants.CENTER);
 		labelMessage.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
 		panel.setOpaque(false);
 		panel.add(labelMessage,BorderLayout.CENTER);
-		panel.setBounds(0, 30, 330, 100);
-
-		setBounds(347,242,330,283);
-		setUndecorated(true);
-		FenetreType fenetre=new FenetreType("images/preparation/validation.png");
-		setContentPane(fenetre);
+		panel.setBounds(0, 30, 330, 100);*/
+		JTextArea description=new JTextArea(message);
+		description.setFont(font);
+		description.setBounds(20, 40, 300, 180);
+		description.setAutoscrolls(true);
+		description.setOpaque(false);
+		description.setLineWrap(true);
+		description.setWrapStyleWord(true);
+		description.setEditable(false);
+		fenetre.add(description);
 
 		getContentPane().setLayout(null);
-		getContentPane().add(panel);
+		//getContentPane().add(panel);
 
 		valider=new Bouton("images/icones/valider.png","images/icones/valider_inv.png");
 		valider.setBounds(40,215,108,44);
@@ -61,7 +72,7 @@ public class FenetreValidation extends JDialog implements ChangeListener{
 		dispose();
 	}
 	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		new FenetreValidation("OUH le joli Message aligné au centre du milieu de la fenêtre").setVisible(true);
 	}
 }
--- trunk/ihm/FenetreWarning.java
+++ trunk/ihm/FenetreWarning.java
@@ -1,10 +1,12 @@
 package ihm;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JDialog;
+import javax.swing.JTextArea;
 
 public class FenetreWarning extends JDialog implements ActionListener{
 	private Bouton warning;
@@ -18,8 +20,21 @@ public class FenetreWarning extends JDialog implements ActionListener{
 		fenetre.setLayout(new FlowLayout());
 		getContentPane().setLayout(null);
 		
+		// Création de la police
+		Font font=new Font("Verdana", Font.BOLD, 12);
+		
+		JTextArea description=new JTextArea(message);
+		description.setFont(font);
+		description.setBounds(20, 40, 300, 180);
+		description.setAutoscrolls(true);
+		description.setOpaque(false);
+		description.setLineWrap(true);
+		description.setWrapStyleWord(true);
+		description.setEditable(false);
+		fenetre.add(description);
+		
 		this.warning=new Bouton("images/icones/warning.png","images/icones/warning_inv.png");
-		this.warning.setBounds(110,212,110,48);
+		this.warning.setBounds(110,222,165,41);
 		fenetre.add(this.warning);
 		this.warning.addActionListener(this);
 		
@@ -29,4 +44,7 @@ public class FenetreWarning extends JDialog implements ActionListener{
 	public void actionPerformed(ActionEvent arg0) {
 		dispose();		
 	}	
+	public static void main(String[] args) {
+		new FenetreWarning("OUH le joli Message aligné au centre du milieu de la fenêtre").setVisible(true);
+	}
 }
